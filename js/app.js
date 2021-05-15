a165df6e6099ea54aed3d5d10f050049711ae0c6
Reza Kelidari <reza.phoenix.1382@gmail.com>
2021-05-15 15:19:50 +0430

    Some Big updates

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
 // Search section
-const search_box = document.querySelector("#search");
+const searchBox = document.querySelector("#search");
 const filters = document.querySelectorAll("form .filters button");
 const result = document.querySelector(".result");
 const noResult = document.querySelector(".no_result");
 const loading = document.querySelector(".loading");
+const searchBoxY = searchBox.getBoundingClientRect().top;
 let iconsList = document.querySelector(".result .icons_list");
 let iconsNumberElement = document.querySelector(".icons-number");
 let filter = 0;
 let icons = [];
 let iconsNumbers;
 
-//Receive icons from json file
 function getIcons() {
+  //Receive icons from json file
   return fetch("icons.json").then((response) => response.json());
 }
 
 async function prepareIcons() {
+  //Making reult section hide for loading
   result.classList.toggle("hide");
   //Receive icons fron getIcon function, which receives them from the json file
   icons = await getIcons();
   iconsNumbers = icons.length;
 
-  //Add icons
   for (i = 0; i < icons.length; i++) {
+    //Add icons
     let code = icons[i].code;
     let tag = icons[i].tag;
 
@@ -30,6 +32,7 @@ async function prepareIcons() {
     iconsList.innerHTML += li;
   }
 
+  //Prepairing result section and hidding loading & noResult section
   loading.classList.toggle("hide");
   result.classList.toggle("hide");
   noResult.className = "no_result hide";
@@ -39,65 +42,92 @@ async function prepareIcons() {
     .querySelector(".result .icons_list")
     .querySelectorAll("li");
 
-  updateResult();
+  updateResultsNumber();
 
-  //Add response to search
-  window.onkeyup = (event) => {
-    if (search_box.value.length == 0) {
-      showAll();
-      updateResult();
+  //Adding a listener to user keyboard keys clicking
+  window.onkeyup = () => {
+    search();
+  };
+
+  //Adding a listener to user page scrolling
+  window.onscroll = () => {
+    console.log(window.scrollY);
+    console.log();
+    if (window.scrollY > searchBoxY) {
+      searchBox.classList.add("scrolled");
     } else {
-      let searchValue = search_box.value;
-      let result = [];
-      searchIndex = searchValue.toUpperCase();
-      for (i = 0; i < iconsList.length; i++) {
-        iconTag = iconsList[i].querySelector(".tag").innerText;
-        iconTagName = iconTag.toUpperCase();
-
-        if (iconTagName.indexOf(searchIndex) > -1) {
-          result.push(i);
-        }
-      }
-
-      result.length > 0 ? show(result) : hideAll();
-      updateResult();
+      searchBox.classList.remove("scrolled");
     }
   };
 
+  //Prepairing filters
   for (i = 0; i < filters.length; i++) {
     let filterButton = filters[i];
     let index = i;
-    filterButton.onclick = () => {
+    filterButton.onclick = (event) => {
+      event.preventDefault();
       filters.forEach((element) => {
         element.className = "";
       });
 
       filter = index;
       filterButton.className = "active";
-      show();
+      search();
     };
   }
 }
 
 prepareIcons();
 
+//The "Search" function is used when searching for an icon or changing filters
+function search() {
+  //Getting search index
+  let index = searchBox.value;
+  if (index !== 0) {
+    //Is the search index not empty?
+    //Making an array for results
+    let result = [];
+    //Making index uppercase to remove case sensitivity
+    let searchIndex = index.toUpperCase();
+    for (i = 0; i < iconsList.length; i++) {
+      //Searching in icons
+      iconTag = iconsList[i].querySelector(".tag").innerText;
+      iconTagName = iconTag.toUpperCase();
+
+      if (iconTagName.indexOf(searchIndex) > -1) {
+        //Does icon match index?
+        result.push(i);
+      }
+    }
+    //If search have result, show the result; otherwise hide all icons
+    result.length > 0 ? show(result) : hideAll();
+  } else {
+    //Or is it empty?
+    showAll();
+  }
+}
+
+//The "Show All" function is used when the page is loaded, the "All" is selected in filters, or the search box is cleared.
 function showAll() {
   for (i = 0; i < iconsList.length; i++) {
     iconsList[i].style.display = "";
   }
   iconsNumbers = icons.length;
-  updateResult();
+  updateResultsNumber();
 }
 
+//The "Hide All" function is used when the search has no results
 function hideAll() {
   for (i = 0; i < iconsList.length; i++) {
     iconsList[i].style.display = "none";
   }
   iconsNumbers = 0;
   noResult.className = "no_result";
-  noResult.innerText = "Your search for \"" + search_box.value + "\" did not return any results";
+  noResult.innerText =
+    'Your search for "' + searchBox.value + '" did not return any results';
 }
 
+//The "Copy" function is used when the user click on an icon
 function copy(index) {
   const copyMessage = document.querySelector(".copied");
   let value = icons[index].code;
@@ -108,50 +138,76 @@ function copy(index) {
   }, 1500);
 }
 
+//The "Show" function used when user search an icon or change filters or both
 function show(index = null) {
+  //Set iconsNumber 0 for updating that
   iconsNumbers = 0;
   if (index == null) {
+    //Is user only changed filters?
     if (filter == 0) {
+      //Is user selected "All" in filters?
       showAll();
     } else {
+      //Or selected another filter?
       for (i = 0; i < iconsList.length; i++) {
+        //Prepare icons as selected filter
         if (icons[i].type == filter) {
+          //Does the icon match the filter?
           iconsList[i].style.display = "";
           iconsNumbers++;
         } else {
+          //Or doesn't?
           iconsList[i].style.display = "none";
         }
       }
     }
-    updateResult();
+    //Then updating numbers of result
+    updateResultsNumber();
   } else {
+    //Or searched an icon or both?
     if (filter == 0) {
+      //Is only searche an icon?
       for (i = 0; i < iconsList.length; i++) {
+        //Prepare icons as search
         if (index.includes(i)) {
+          //Does icon match the search index?
           iconsList[i].style.display = "";
           iconsNumbers++;
         } else {
+          //Or doesn't?
           iconsList[i].style.display = "none";
         }
       }
     } else {
+      //Or both of searching icon and changing filter?
       for (i = 0; i < iconsList.length; i++) {
+        //Prepare icons as search and filter
         if (index.includes(i) && icons[i].type == filter) {
+          //Does icon match the search index and filter?
           iconsList[i].style.display = "";
           iconsNumbers++;
         } else {
+          //Or doesn't?
           iconsList[i].style.display = "none";
         }
       }
     }
   }
-  updateResult();
+  //Then updating numbers of result
+  updateResultsNumber();
 }
 
-function updateResult() {
+function updateResultsNumber() {
+  //Updating numbers of icons
+  //Setting numbers of icons
   iconsNumberElement.innerText = "Result: " + iconsNumbers + " Icons";
   if (iconsNumbers === 0) {
-    hideAll()
+    //Is no icon displayed?
+    hideAll();
+  } else {
+    //Or displayed?
+    //Making noResult section hide
+    noResult.className = "no_result hide";
   }
 }
 // End Search section
